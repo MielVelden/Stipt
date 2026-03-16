1cf1256f854774d4901a0d5476a6b789c7203d34
pepijnemmers <pepijn.emmers@hotmail.com>
2026-03-16 23:17:45 +0100

    implemented dropdown menu in table

---
+++ Backoffice/app/components/dropdown-menu.tsx
@@ -0,0 +1,62 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
+import { EllipsisVerticalIcon } from "lucide-react"
+import { Link } from "react-router"
+import { Button } from "./ui/button"
+
+type actionDropdown = {
+  label: string
+  icon?: React.ReactNode
+  variant?: "default" | "destructive"
+  separatorBefore?: boolean
+  linkTo?: string
+}
+
+export default function createActionsDropdown({
+  actions,
+}: {
+  actions: actionDropdown[]
+}) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex size-8 cursor-pointer text-muted-foreground data-[state=open]:bg-muted"
+          size="icon"
+        >
+          <EllipsisVerticalIcon />
+          <span className="sr-only">Open menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-fit">
+        {actions.map((action, index) => (
+          <div key={`${action.label}-${index}`}>
+            {action.separatorBefore && <DropdownMenuSeparator />}
+            <DropdownMenuItem
+              variant={action.variant}
+              asChild={!!action.linkTo}
+            >
+              {action.linkTo ? (
+                <Link to={action.linkTo}>
+                  {action.icon && <span className="ml-2">{action.icon}</span>}
+                  {action.label}
+                </Link>
+              ) : (
+                <>
+                  {action.icon && <span className="ml-2">{action.icon}</span>}
+                  {action.label}
+                </>
+              )}
+            </DropdownMenuItem>
+          </div>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
--- Backoffice/app/routes/dashboard/dashboard.overview.tsx
+++ Backoffice/app/routes/dashboard/dashboard.overview.tsx
@@ -2,8 +2,57 @@ import { PageHeader } from "~/layouts/components/page-header"
 import { PageContainer } from "~/layouts/components/page-container"
 import { Link } from "react-router"
 import { Button } from "~/components/ui/button"
+import { DataTable } from "~/components/data-table"
+import type { ColumnDef } from "@tanstack/react-table"
+import createActionsDropdown from "~/components/dropdown-menu"
+import { EyeIcon, TrashIcon } from "lucide-react"
+
+type Event = {
+  id: number
+  name: string
+}
+
+const columns: ColumnDef<Event>[] = [
+  { accessorKey: "id", header: "ID" },
+  { accessorKey: "name", header: "Full Name" },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return createActionsDropdown({
+        actions: [
+          {
+            label: "Bekijk",
+            icon: <EyeIcon />,
+            linkTo: `/app/evenementen/${row.getValue("id")}`,
+          },
+          {
+            label: "Verwijder",
+            icon: <TrashIcon />,
+            separatorBefore: true,
+            variant: "destructive",
+            linkTo: `/app/evenementen/${row.getValue("id")}/verwijderen`,
+          },
+        ],
+      })
+    },
+  },
+]
 
 export default function Page() {
+  const events: Event[] = [
+    { id: 1, name: "Event 1" },
+    { id: 2, name: "Event 2" },
+    { id: 3, name: "Event 3" },
+    { id: 4, name: "Event 4" },
+    { id: 5, name: "Event 5" },
+    { id: 6, name: "Event 6" },
+    { id: 7, name: "Event 7" },
+    { id: 8, name: "Event 8" },
+    { id: 9, name: "Event 9" },
+    { id: 10, name: "Event 10" },
+    { id: 11, name: "Event 11" },
+  ]
+
   return (
     <>
       <PageHeader title="Dashboard" />
@@ -12,6 +61,10 @@ export default function Page() {
         <Button className="mt-2" asChild>
           <Link to="/app/evenementen">Bekijk de evenementen</Link>
         </Button>
+
+        <div className="mt-6">
+          <DataTable data={events} columns={columns} />
+        </div>
       </PageContainer>
     </>
   )
