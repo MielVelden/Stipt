c6bb7675f9159510cbb4fc4560fe4d1b830e0cf7
Pepijn Emmers <pepijn.emmers@hotmail.com>
2026-06-01 22:24:07 +0200

    typo fix

--- Backend/Backend.Web/Features/EventParticipants/Exceptions/InviteExpiredException.cs
+++ Backend/Backend.Web/Features/EventParticipants/Exceptions/InviteExpiredException.cs
@@ -2,4 +2,3 @@ namespace Backend.Web.Features.EventParticipants.Exceptions;
 
 public sealed class InviteExpiredException()
     : Exception("Deze uitnodigingscode is verlopen. Neem contact op met de organisatie voor een nieuwe uitnodiging.");
-n
